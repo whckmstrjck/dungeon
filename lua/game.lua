dfce69f14bfbc6d38b6200f3e1ac3dac66cbe606
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-11-22 14:05:00 +0100

    Add additional goblin monster to game initialization

--- lua/game.lua
+++ lua/game.lua
@@ -4,7 +4,8 @@ Game = Class:new({
     tbl.player = Player:new({ x = 18, y = 14 })
     tbl.camera = Camera:new({ player = tbl.player })
     tbl.monsters = {
-      Goblin:new({ x = 23, y = 20 })
+      Goblin:new({ x = 23, y = 20 }),
+      Goblin:new({ x = 26, y = 20 })
     }
 
     return setmetatable(tbl or {}, { __index = _ENV })
